dccf3b54fc79143ee68994ca1dc8a106b7285e64
Satyam Singh <satyamsingh@Mac.lan>
2025-03-21 15:04:17 +0530

    Initial commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,10 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://www.flipkart.com/')
+
+
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get('.Pke_EE').clear('l');
+    cy.get('.Pke_EE').type('laptop{enter}');
+  })
+})
